715cf9a4cca8e1bbd54e762c3993cdecf9bfd50f
Ilya <ilya.yesipov2007@gmail.com>
2025-06-30 16:31:28 +0300

    Update 1.4.c

--- 1.4.c
+++ 1.4.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+main()
+{
+	printf("Celsius to fahrenhate Table\n");
 
+	float fahr, celsius;
+	int lower, upper, step;
+
+	lower = 0;
+	upper = 300;
+	step = 20;
+
+	celsius = lower;
+
+		while (celsius <= upper)
+		{
+			fahr = (celsius * (9.0 / 5.0) + 32);
+			printf("%3.0f %6.1f\n", celsius, fahr);
+			celsius = celsius + step;
+		}
+}
