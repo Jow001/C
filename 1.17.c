918b96449f5a0c34b6769c9c2241be849e6bffa7
Ilya <ilya.yesipov2007@gmail.com>
2025-07-16 13:48:12 +0300

    Update 1.17.c

--- 1.17.c
+++ 1.17.c
@@ -2,62 +2,38 @@
 #define MAXLINE 1000
 
 int getline(char line[], int maxline);
-void copy(char to[], char fron[]);
 
-main()
+int main()
 {
-	int len;
-	int max;
-	int continueline;
-	char line[MAXLINE];
-	char longest[MAXLINE];
+    int len;
+    char line[MAXLINE];
 
-	max = 0;
+    while ((len = getline(line, MAXLINE)) > 0)
+    {
+        if (len > 80)
+        {
+            printf("%s", line);
+        }
+    }
 
-	while((len = getline(line, MAXLINE)) > 0)
-	{
-		if (len > max)
-		{
-			max = len;
-			copy(longest, line);
-		}
-	}
-
-	if (max > 80)
-	{
-		printf("%s", longest);
-	}
-
-	return 0;
+    return 0;
 }
 
-int getline(char s[], int lim)
+int getline(char line[], int lim)
 {
-	int c, i;
+    int str, i;
 
-	for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; i++)
-	{
-		s[i] = c;
-	}
+    for (i = 0; i < lim - 1 && (c = getchar()) != EOF && str != '\n'; i++)
+    {
+        line[i] = str;
+    }
 
-	if (c == '\n')
-	{
-		s[i] = c;
-		i++;
-	}
+    if (str == '\n')
+    {
+        line[i] = str;
+        i++;
+    }
 
-	s[i] = '\0';
-	return i;
-}
-
-void copy(char to[], char from[])
-{
-	int i;
-
-	i = 0;
-
-	while ((to[i] = from[i]) != '\0')
-	{
-		i++;
-	}
+    line[i] = '\0';
+    return i;
 }
