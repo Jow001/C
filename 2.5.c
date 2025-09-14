aac5968b09ba8c1960deab90bc2eb02ff3fe555c
Ilya <ilya.yesipov2007@gmail.com>
2025-09-14 15:35:31 +0200

    Update 2.5.c

--- 2.5.c
+++ 2.5.c
@@ -1,1 +1,60 @@
+#include <stdio.h>
 
+int main() 
+{    
+    char s1[99];
+    char s2[99];
+    int str;
+    int i = 0;
+    int j = 0;
+    int i1 = 0;
+    int j1 = 0;
+    int somethingwas = 0;
+    int error = -1;
+
+    while ((str = getchar()) != EOF)
+    {
+        s1[i] = str;
+        i++;
+    }
+    
+    i = 0;
+
+    while ((str = getchar()) != EOF)
+    {
+        s2[i] = str;
+        i++;
+    }
+
+    i = 0;
+
+    while (s2[i] != '\0')
+    {
+        while (s1[j] != '\0')
+        {
+
+            if (s2[i] == s1[j])
+            {
+                printf("%c : %i \n", s2[i], j);
+                j++;
+                somethingwas = 1;
+                break;
+            }
+            else
+            {
+                j++;
+            }
+
+        }
+
+        i++;
+    }
+
+    if (somethingwas == 0)
+    {
+        printf("%i", error);
+    }
+
+    return 0;
+
+}
